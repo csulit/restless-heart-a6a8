205b0f119c4d5249e5b761e884d46282b2e5b119
casulit <chrisgelosulit@gmail.com>
2024-08-06 06:38:17 +0800

    feat(webhooks): Add property proximity query function

--- src/routes/webhooks.ts
+++ src/routes/webhooks.ts
@@ -1,12 +1,57 @@
 import { Hono } from "hono";
 import db from "@/database";
-import { user } from "@/database/schema";
+import { property } from "@/database/schema";
+import { and, between, sql } from "drizzle-orm";
 
 const app = new Hono();
 
+interface StDistanceSphereArgs {
+  minLat: number;
+  maxLat: number;
+  minLong: number;
+  maxLong: number;
+  lat: number;
+  long: number;
+  distanceInMiles: number;
+}
+
+const stDistanceSphere = ({
+  minLat,
+  maxLat,
+  minLong,
+  maxLong,
+  lat,
+  long,
+  distanceInMiles,
+}: StDistanceSphereArgs) => {
+  return db
+    .select()
+    .from(property)
+    .where(
+      and(
+        between(property.latitude, minLat, maxLat),
+        between(property.longitude, minLong, maxLong),
+        sql`ST_distance_sphere(
+          point(${long}, ${lat}), 
+          point(${property.longitude}, ${property.latitude})
+        ) * 0.000621371192 < ${distanceInMiles}`
+      )
+    )
+    .limit(100);
+};
+
 app.get("/", async (c) => {
-  const users = await db.select().from(user);
-  return c.json({ message: "Hello webhooks", users });
+  const testArgs: StDistanceSphereArgs = {
+    minLat: 34.0,
+    maxLat: 36.0,
+    minLong: -118.0,
+    maxLong: -116.0,
+    lat: 34.052235,
+    long: -118.243683,
+    distanceInMiles: 10,
+  };
+  const properties = await stDistanceSphere(testArgs);
+  return c.json({ message: "Hello webhooks", properties });
 });
 
 app.post("/properties", async (c) => {
